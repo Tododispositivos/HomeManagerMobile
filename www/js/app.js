a366bd5a93ae6f21eab713aa7f81ba8c6db6ef82
Tododispositivos <informaciones@tododispositivos.cl>
2015-12-12 12:03:50 -0300

    Actualización
    Incluye Modal de formulario de ingreso de movimientos (no funcional aun)
    y Loading de identificación de Usuario

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,5 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('HomeManagerMobile', ['ionic'])
-
+ 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -18,10 +13,10 @@ angular.module('HomeManagerMobile', ['ionic'])
     console.log('Plataforma: ',ionic.Platform.platform());
     console.log('Versión: ',ionic.Platform.version());
   });
-}) 
-
+})
+ 
 .config(function($stateProvider, $urlRouterProvider) {
-
+ 
   $stateProvider
     .state('admin', {
       url: '/admin',
@@ -97,13 +92,13 @@ angular.module('HomeManagerMobile', ['ionic'])
     })
    $urlRouterProvider.otherwise('/sign-in');
 })
-
+ 
 .config(function($sceDelegateProvider) {
   $sceDelegateProvider.resourceUrlWhitelist([
     // Allow All.
     '*'
   ])})
-
+ 
 .factory('DatosUsuario', function() {
   return {
       rut :'',
@@ -115,10 +110,12 @@ angular.module('HomeManagerMobile', ['ionic'])
       email:''
   };
 })
-
-.controller('SignInCtrl', function($scope, $state, $http, DatosUsuario, $ionicPopup, $timeout) {
-  
+ 
+.controller('SignInCtrl', function($scope, $state, $http, DatosUsuario, $ionicPopup, $timeout, $ionicLoading) {
+ 
   $scope.signIn = function(user) {
+  $timeout( function() {
+    $scope.show($ionicLoading);
     $http.get("http://www.monroy-ramirez.cl/HomeManager/ws/ws_query.php", {params:{cod_query:'login', login_user:user.username, login_pass:user.password}})
     .then(function (response) {
       DatosUsuario.rut    = response.data[0].per_rut;
@@ -127,39 +124,50 @@ angular.module('HomeManagerMobile', ['ionic'])
       DatosUsuario.por    = response.data[0].per_por;
       DatosUsuario.nivel  = response.data[0].per_nivel;
       DatosUsuario.familia= response.data[0].cod_familia;
-      DatosUsuario.email  = response.data[0].per_email1;   
-      DatosUsuario.cuenta = '';      
-      $scope.data = response.data;      
+      DatosUsuario.email  = response.data[0].per_email1;  
+      DatosUsuario.cuenta = '';     
+       $scope.hide($ionicLoading); 
+      $scope.data = response.data;     
       if(response.data[0].per_rut == user.username) {
        console.log('SignInCtrl');
        console.log('---Usuario OK.');
-       $state.go('tabs.home');     
+       $state.go('tabs.home');    
       }
       else {
-       $scope.showPopup();
-       //alert('Usuario Incorrecto!'); 
+       $scope.showPopup('Rut o contraseña incorrecta.');
+       //alert('Usuario Incorrecto!');
         return;
      }
-     })    
+     }, function(reason) {
+       console.log('Error en servicio de autenticación: ' +reason.value);
+       $scope.showPopup('No se pudieron validar los datos.');
+       $scope.hide($ionicLoading); 
+      })
+      $scope.$broadcast('scroll.refreshComplete');
+      },1000);
+      $scope.hide($ionicLoading); 
   }
-
-
- $scope.showPopup = function() {
+  $scope.showPopup = function(text) {
    $scope.data = {}
    var myPopup = $ionicPopup.show({
-    template: "<style>.popup { color:#000000;}</style>Rut o contraseña incorrecta."});
-   myPopup.then(function(res) {
-     console.log('Tapped!', res);
+    template: "<h4 class'popup'>"+text+".</h4>"});
+    myPopup.then(function(res) {
+    console.log('Tapped!', res);
    });
    $timeout(function() {
       myPopup.close(); //close the popup after 3 seconds for some reason
    }, 3000);
   };
+  $scope.show = function() {
+    $ionicLoading.show({
+      template: '<p>Identificando...</p><ion-spinner></ion-spinner>'
+    });
+  };
+  $scope.hide = function(){
+    $ionicLoading.hide();
+  };
 })
-
-  
-
-
+ 
 .controller('HomeTabCtrl', function($scope, DatosUsuario, $ionicSlideBoxDelegate) {
   $scope.rut = DatosUsuario.rut;
   $scope.nombre = DatosUsuario.nombre;
@@ -183,9 +191,36 @@ angular.module('HomeManagerMobile', ['ionic'])
   };
   $scope.update = function() {
     $ionicSlideBoxDelegate.update();
-  }; 
-  
+  };
+})
  
+.controller('ModalCtrl', function($scope, $ionicModal) {
+  //debugger;
+   $scope.fecha = new Date();
+   $ionicModal.fromTemplateUrl('templates/ingreso_movimientos.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
 })
 
 .controller('CuentasTabCtrl', function($scope, $state, DatosUsuario, $http, $timeout, $ionicSlideBoxDelegate) {
@@ -200,46 +235,59 @@ angular.module('HomeManagerMobile', ['ionic'])
   };
   $scope.update = function() {
     $ionicSlideBoxDelegate.update();
-  }; 
-  $scope.getCuentas = function() { 
+  };
+  $scope.getCuentas = function() {
      console.log('getCuentas()');
      $scope.getCuentasFamiliares();
      $scope.getMisCuentas();
      };
-  $scope.getCuentasFamiliares = function() { 
+  $scope.getCuentasFamiliares = function() {
     $http.get("http://www.monroy-ramirez.cl/HomeManager/ws/ws_query.php", {params:{cod_query:'cuentasXfamilia', user_fam:DatosUsuario.familia}})
     .then(function (response) {
       console.log('getCuentasFamiliares()');
-      $scope.dataq4 = response.data;    
-      $scope.date = new Date(); 
-      $ionicSlideBoxDelegate.$getByHandle('image-viewer').update(); 
+      $scope.dataq4 = response.data;   
+      $scope.date = new Date();
+      $ionicSlideBoxDelegate.$getByHandle('image-viewer').update();
     })};
-  $scope.getMisCuentas = function() { 
+  $scope.getMisCuentas = function() {
    $http.get("http://www.monroy-ramirez.cl/HomeManager/ws/ws_query.php", {params:{cod_query:'cuentasXrut', login_user:DatosUsuario.rut}})
       .then(function (response) {
         console.log('getMisCuentas()');
-        $scope.dataq3 = response.data;      
-        $ionicSlideBoxDelegate.$getByHandle('image-viewer').update(); 
+        $scope.dataq3 = response.data;     
+        $ionicSlideBoxDelegate.$getByHandle('image-viewer').update();
        })};
   $scope.refreshCuentas = function() {
     console.log('refreshCuentas()');
     $timeout( function() {
       $scope.getCuentas();
-      //$scope.getCuentasFamiliares();
-      //$scope.getMisCuentas();
-      //Stop the ion-refresher from spinning
       $scope.$broadcast('scroll.refreshComplete');
-    }, 1000);     
+    }, 1000);    
   };
   $scope.getDetalleCuenta = function(cuenta) { 
    $scope.date = new Date();
+   $scope.datadetalleXcuenta = null; 
+   console.log('getDetalleCuenta()');
+   console.log('Fecha Actual = '+$scope.date);
    DatosUsuario.cuenta = cuenta;
+   $scope.cuenta = cuenta;
    console.log('DatosUsuario.cuenta ='+DatosUsuario.cuenta);
-   $scope.mesActual = $scope.date.getMonth();
-   $scope.anoActual = $scope.date.getFullYear();
+   console.log('typeof($scope.consultafecha) ='+typeof($scope.consultafecha));
+ 
+   if (typeof($scope.consultafecha) != 'undefined' && $scope.consultafecha != null) {
+       $scope.mesActual = eval("$scope.consultafecha.getMonth() + 1");
+       $scope.anoActual = $scope.consultafecha.getFullYear();
+    }
+    else {
+      $scope.mesActual =  eval("$scope.date.getMonth() + 1");
+      $scope.anoActual = $scope.date.getFullYear();
+      }
+    console.log('Mes a Consultar ='+$scope.mesActual);
+    console.log('Año a Consultar ='+$scope.anoActual);
    $http.get("http://www.monroy-ramirez.cl/HomeManager/ws/ws_query.php", {params:{cod_query:'detalleXcuenta', cod_cuenta:cuenta, cod_ano:$scope.anoActual, cod_mes:$scope.mesActual}})
       .then(function (response) {
-        $scope.datadetalleXcuenta = response.data;      
+        $scope.datadetalleXcuenta = response.data;     
+        $ionicSlideBoxDelegate.$getByHandle('image-viewer').update();
+       //console.log('Retorno desde servicio ='+$scope.datadetalleXcuenta);
     })};
   $scope.refreshDetalleCuenta = function() {
       console.log('refreshDetalleCuenta('+DatosUsuario.cuenta+')');
@@ -247,62 +295,14 @@ angular.module('HomeManagerMobile', ['ionic'])
         $scope.getDetalleCuenta(DatosUsuario.cuenta);
         //Stop the ion-refresher from spinning
         $scope.$broadcast('scroll.refreshComplete');
-      }, 1000);     
+      }, 1000);    
   };
 })
-
+ 
 .controller('bancoCtrl', function($scope, $http, DatosUsuario) {
     $http.get("http://www.monroy-ramirez.cl/HomeManager/ws/ws_query.php", {params:{cod_query:'bancosXfamilia', user_fam:DatosUsuario.familia}})
     .then(function (response) {
       console.log('bancoCtrl');
-      $scope.data = response.data;      
+      $scope.data = response.data;     
     });
 })
-
-.controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
-   $scope.attendees = [
-    { firstname: 'Nicolas', lastname: 'Cage' },
-    { firstname: 'Jean-Claude', lastname: 'Van Damme' },
-    { firstname: 'Keanu', lastname: 'Reeves' },
-    { firstname: 'Steven', lastname: 'Seagal' }
-  ];
-  $scope.toggleLeft = function() {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
-})
-
-/*.controller('PDFCtrl', [ '$scope', 'PDFViewerService', function($scope, pdf) {
-    $scope.viewer = pdf.Instance("viewer");
-    $scope.nextPage = function() {
-        $scope.viewer.nextPage();
-    };
-    $scope.prevPage = function() {
-        $scope.viewer.prevPage();
-    };
-    $scope.pageLoaded = function(curPage, totalPages) {
-        $scope.currentPage = curPage;
-        $scope.totalPages = totalPages;
-    };
-}])
-
-.controller('TestController', [ '$scope', 'PDFViewerService', '$sce', function($scope, pdf, $sce) {
-  console.log('TestController: new instance');
-  $scope.pdfURL = $sce.trustAsResourceUrl("http://www.monroy-ramirez.cl/HomeManager/angular/Invoice.pdf");
-  $scope.instance = pdf.Instance("viewer");
-  $scope.nextPage = function() {
-    $scope.instance.nextPage();
-  };
-  $scope.prevPage = function() {
-    $scope.instance.prevPage();
-  };
-  $scope.gotoPage = function(page) {
-    $scope.instance.gotoPage(page);
-  };
-  $scope.pageLoaded = function(curPage, totalPages) {
-    $scope.currentPage = curPage;
-    $scope.totalPages = totalPages;
-  };
-  $scope.loadProgress = function(loaded, total, state) {
-    console.log('loaded =', loaded, 'total =', total, 'state =', state);
-  };
-}])*/
